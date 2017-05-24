a954bb76f4f5c83fa5414ae64b226523c46f288e
Matt MacNeil <matt@mattmacneil.com>
2017-05-24 17:36:07 -0400

    Added navigation elements, positioning. Gitignore updated.

--- wp-content/themes/machineguntheme/template-parts/nav.php
+++ wp-content/themes/machineguntheme/template-parts/nav.php
@@ -1,10 +1,32 @@
-<?php
-wp_nav_menu( array( 
-    'theme_location' 	=> 'mg-nav',
-    'items_wrap' 			=> '%3$s',
-    'menu_class' 			=> '',
-    'menu_id'         => FALSE,
-    'item_spacing' 		=> discard,
-    'depth'           => 1,
-    'container_class' => 'machine-gun-menu' ) ); 
-?>
+<div class="nav-container">
+
+	<div class="content-wrapper">
+
+		<nav>
+
+			<a href="<?php echo home_url(); ?>">
+			<div class="nav-logo" style="background-image: url('<?php bloginfo('template_url'); ?>/assets/images/nav-logo.png');">
+
+					<em>Machine Gun Studios</em>
+
+			</div><!-- nav logo -->
+			</a>
+
+			<?php
+			wp_nav_menu( array( 
+			    'theme_location' 	=> 'mg-nav',
+			    'items_wrap' 			=> '<div class="menu-aligner">%3$s</div>',
+			    'menu_class' 			=> '',
+			    'menu_id'         => FALSE,
+			    'item_spacing' 		=> discard,
+			    'depth'           => 1,
+			    'container_class' => 'machine-gun-menu' ) ); 
+			?>
+
+			<div class="clearfix"></div>
+
+		</nav><!-- nav -->
+
+	</div><!-- content wrapper -->
+
+</div><!-- nav container -->

141f66573e87a1a30d8a47075a9f5c508fc48ddf
Matt MacNeil <matt@mattmacneil.com>
2017-05-28 14:23:39 -0400

    Added recording and mixing content.

--- wp-content/themes/machineguntheme/recording.php
+++ wp-content/themes/machineguntheme/recording.php
@@ -8,5 +8,50 @@
 
 	get_template_part( 'template-parts/hero' );
 
+?>
+
+	<div class="doubleside">
+
+		<div class="content-wrapper">
+
+			<div class="col-content">
+
+				<div class="col-content-one">
+
+					<span>&raquo;</span>
+
+				</div><!-- one -->
+
+				<div class="col-content-two">
+
+					<?php
+
+						$recordmixcontent = get_post_meta( get_the_ID(), 'recordmixcontent', true);
+						if( ! empty( $recordmixcontent ) ) {
+							echo $recordmixcontent;
+						}
+							else {
+								echo '<h1>All The Latest From Machine Gun Studios.</h1>';
+							}
+
+					?>
+
+				</div><!-- two -->
+
+				<div class="col-content-three">
+
+					<?php get_sidebar(); ?>
+
+				</div><!-- three -->
+
+				<div class="clearfix"></div>
+
+			</div><!-- rap -->
+
+		</div><!-- content wrapper -->
+
+	</div><!-- doubleside -->
+
+<?php
 	get_footer(); 
 ?>

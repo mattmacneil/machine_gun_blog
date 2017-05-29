8bd01a5218d699389e8fbe251c373c73753fc5e8
Matt MacNeil <matt@mattmacneil.com>
2017-05-29 15:30:27 -0400

    Changed hero landing bars to absolute.

--- wp-content/themes/machineguntheme/template-parts/hero.php
+++ wp-content/themes/machineguntheme/template-parts/hero.php
@@ -1,6 +1,30 @@
-<div class="hero">
 
-	<div class="hero-overlay">
+<?php
+
+	$checkstudio_slug = basename(get_permalink());
+	$studio_trans = NULL;
+
+	if ($checkstudio_slug == "studio") {
+		$studio_trans = "background-color: rgba(34, 83, 120, 0.4);";
+	}
+			else $studio_trans = NULL;
+
+?>
+
+<div class="hero" style="background-image: url('<?php echo the_post_thumbnail_url(); ?>');">
+
+	<?php
+
+		$page_name_slug = basename(get_permalink());
+		$hero_trans_class = NULL;
+
+		if ($page_name_slug == "mgwp" || $page_name_slug == "studio" || $page_name_slug == "contact") {
+				$hero_trans_class = "hero-transparent";
+		}
+			else $hero_trans_class = NULL;
+	?>
+
+	<div class="hero-overlay <?php echo $hero_trans_class; ?>" style="<?php echo $studio_trans; ?>">
 
 		<div class="nav-blocker"></div><!-- nav blocker -->
 
@@ -17,18 +41,18 @@
 					<div class="hero-col-content-two">
 
 						<div class="hero-burst">
-
-							HI
-
 						</div><!-- hero burst -->
 
+						<div class="hero-snipe">
+						</div><!-- hero snipe -->
+
 						<?php 
-/*
+
 							if ( is_front_page() ) {	
-								echo '<img src="';
+								echo '<div class="landing-bars"><img src="';
 								echo bloginfo('template_url');
-								echo '/assets/images/landing-bars.gif">'; 
-   						} */
+								echo '/assets/images/landing-bars.gif"></div><!-- landing-bars -->'; 
+   						}
 
 							$pageheadline = get_post_meta( get_the_ID(), 'headliner', true);
 							if( ! empty( $pageheadline ) ) {
@@ -62,30 +86,9 @@
 
 
 
-		<?php
-
-		$checkstudio_slug = basename(get_permalink());
-		$studio_trans = NULL;
-
-			if ($checkstudio_slug == "studio") {
-				$studio_trans = "background-color: rgba(34, 83, 120, 0.4);";
-			}
-					else $studio_trans = NULL;
-		?>
-
 
 	<div class="hero" style="background-image: url('<?php echo the_post_thumbnail_url(); ?>');">
 
-		<?php
-
-		$page_name_slug = basename(get_permalink());
-		$hero_trans_class = NULL;
-
-			if ($page_name_slug == "mgwp" || $page_name_slug == "studio" || $page_name_slug == "contact") {
-				$hero_trans_class = "hero-transparent";
-			}
-					else $hero_trans_class = NULL;
-		?>
 
 		<div class="hero-overlay <?php echo $hero_trans_class; ?>" style="<?php echo $studio_trans; ?>">
 

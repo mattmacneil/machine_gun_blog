65d7f41494ab1de28d48845e84fa67fac4bca61c
Matt MacNeil <matt@mattmacneil.com>
2017-06-01 18:40:13 -0400

    Placed loop into blog page (unstyled).

--- wp-content/themes/machineguntheme/blog.php
+++ wp-content/themes/machineguntheme/blog.php
@@ -8,6 +8,18 @@
 
 	<?php get_template_part( 'template-parts/hero' ); ?>
 
-	hello
+	<div style="height: 100px;"></div>
+
+<?php $custom_query = new WP_Query('cat=-999999999'); // exclude category 9
+while($custom_query->have_posts()) : $custom_query->the_post(); ?>
+
+	<div <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+		<h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+		<?php the_content(); ?>
+	</div>
+
+<?php endwhile; ?>
+<?php wp_reset_postdata(); // reset the query ?>
+	<div style="height: 100px;"></div>
 
 	<?php get_footer(); ?>

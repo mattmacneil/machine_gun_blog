bdc1bf26f3a5b282f9e463cff402e2394d822af2
Matt MacNeil <matt@mattmacneil.com>
2017-06-01 23:59:59 -0400

    Removed test loop on blog page.

--- wp-content/themes/machineguntheme/blog.php
+++ wp-content/themes/machineguntheme/blog.php
@@ -12,65 +12,6 @@
 
 		<div class="blog-card-container">
 
-			<?php $custom_query = new WP_Query('cat=-42069');
-			while($custom_query->have_posts()) : $custom_query->the_post(); ?>
-
-			<div class="blog-card">
-
-				<div class="blog-card-left">
-
-					<div class="blog-card-left-inner">
-
-						<div class="blog-card-image-container">
-
-							<div class="blog-card-image" style="background-image: url('<?php the_post_thumbnail_url(); ?>');">
-
-								<div class="blog-card-image-overlay">
-								</div><!-- blog card image overlay -->
-
-							</div><!-- blog card image -->
-
-						</div><!-- blog card image -->
-
-						<div class="blog-card-date">
-
-							<?php the_time('dMy'); ?>
-
-						</div><!-- blog card date -->
-
-					</div><!-- blog card left-inner -->
-
-				</div><!-- blog card left -->
-
-				<div class="blog-card-right">
-
-					<a href="<?php the_permalink(); ?>" class="title">
-						<?php the_title(); ?>
-					</a>
-
-					<em>By: <?php the_author(); ?></em>
-
-					<?php $blog_card_content = get_the_content(); echo mb_strimwidth($blog_card_content, 0, 175, '...');?>
-
-					<a href="<?php the_permalink(); ?>" class="noline blog-card-button">read it &raquo;</a>
-
-				</div><!-- blog card right -->
-
-			</div><!-- blog card -->
-
-			<?php endwhile; ?>
-			<?php wp_reset_postdata(); ?>
-
-		</div><!-- blog card container -->
-
-</div><!-- content wrapper -->
-
-
-
-	<div style="height: 100px;"></div>
-
-<hr />
-
 <?php
   $paged = ( get_query_var('paged') ) ? get_query_var('paged') : 1;
   $query_args = array(
@@ -133,11 +74,11 @@
 	    <div class="blog-page-links">
 
 	      <span class="newer">
-	      <?php echo get_previous_posts_link( 'Newer Entries' ); link ?>
+	      <?php echo get_previous_posts_link( '&laquo; newer shit' ); link ?>
 	      </span>
 
 	      <span class="older">
-	      	<?php echo get_next_posts_link( 'Older Entries', $the_query->max_num_pages ); ?>
+	      	<?php echo get_next_posts_link( 'older shit &raquo;', $the_query->max_num_pages ); ?>
 	      </span>
 
 	      <div class="clearfix"></div>
@@ -153,8 +94,8 @@
 	  </article>
 	<?php endif; ?>
 
-<hr />
+		</div><!-- blog card container -->
 
-	<div style="height: 100px;"></div>
+	</div><!-- content wrapper -->
 
 	<?php get_footer(); ?>

f2e8327542795df797be3149ce59b25ff67f82e7
Matt MacNeil <matt@mattmacneil.com>
2017-06-01 20:30:17 -0400

    Added blog part components, image area.

--- wp-content/themes/machineguntheme/blog.php
+++ wp-content/themes/machineguntheme/blog.php
@@ -10,23 +10,44 @@
 
 	<div class="blog-card-container">
 
+		<a href="xxxx" class="noline">
 		<div class="blog-card">
 
 			<div class="blog-card-left">
 
-				hi
+				<div class="blog-card-left-inner">
+
+					<div class="blog-card-image">
+
+						hi
+
+					</div><!-- blog card image -->
+
+					<div class="blog-card-date">
+
+						hi
+
+					</div><!-- blog card date -->
+
+				</div><!-- blog card left-inner -->
 
 			</div><!-- blog card left -->
 
 			<div class="blog-card-right">
 
-				<p class="pslug">hi</p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 
 			</div><!-- blog card right -->
 
 		</div><!-- blog card -->
 
 	</div><!-- blog card container -->
+	</a>
 
 
 	<div style="height: 100px;"></div>

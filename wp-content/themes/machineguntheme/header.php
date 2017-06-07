12a0886f2854a7c608b97f5145705be5ae8b66f0
Matt MacNeil <matt@mattmacneil.com>
2017-06-07 13:43:28 -0400

    Implemented Google Analytics w/ tracking code.

--- wp-content/themes/machineguntheme/header.php
+++ wp-content/themes/machineguntheme/header.php
@@ -17,6 +17,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
   	<link href="<?php echo get_bloginfo('template_directory'); ?>/style.css" rel="stylesheet">
 
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-100579821-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
     <?php wp_head();?>
     
   </head>

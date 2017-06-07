12b8625bcd6378801da83c122806044de21f2c2c
Matt MacNeil <matt@mattmacneil.com>
2017-06-07 16:11:00 -0400

    Settled on video bg for landing hero.

--- wp-content/themes/machineguntheme/template-parts/hero-video.php
+++ wp-content/themes/machineguntheme/template-parts/hero-video.php
@@ -3,8 +3,8 @@
 		<div class="hero-video">
 
 			<video autoplay muted loop>
-		    <source src="<?php bloginfo('template_url'); ?>/assets/images/video/hero-video-6.webm" type="video/webm">
-		    <source src="<?php bloginfo('template_url'); ?>/assets/images/video/hero-video-6.mp4" type="video/mp4">
+		    <source src="<?php bloginfo('template_url'); ?>/assets/images/video/hero-video.webm" type="video/webm">
+		    <source src="<?php bloginfo('template_url'); ?>/assets/images/video/hero-video.mp4" type="video/mp4">
 			</video>
 
 		</div><!-- hero video -->
